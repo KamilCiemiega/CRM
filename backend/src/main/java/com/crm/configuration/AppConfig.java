125c28cd9a83c5205bf19cc85a9ecdc08e36ad60
KamilCiemiega <kamil13241012@interia.pl>
2024-09-06 15:34:56 +0200

    change modelDtoMapper configuration

--- backend/src/main/java/com/crm/configuration/AppConfig.java
+++ backend/src/main/java/com/crm/configuration/AppConfig.java
@@ -1,9 +1,6 @@
 package com.crm.configuration;
 
-import com.crm.controller.dto.UserDto;
-import com.crm.entity.User;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,32 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        modelMapper.addMappings(new PropertyMap<UserDto, User>() {
-            @Override
-            protected void configure() {
-                map().setFirstName(source.getFirstName());
-                map().setLastName(source.getLastName());
-                map().setEmail(source.getEmail());
-                map().setRole(source.getRole());
-                map().setId(source.getId());
-            }
-        });
-
-        modelMapper.addMappings(new PropertyMap<User, UserDto>() {
-            @Override
-            protected void configure() {
-                map().setFirstName(source.getFirstName());
-                map().setLastName(source.getLastName());
-                map().setEmail(source.getEmail());
-                map().setRole(source.getRole());
-                map().setId(source.getId());
-            }
-        });
-
-        return modelMapper;
+        return new ModelMapper();
     }
-
 }
-
